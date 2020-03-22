e13c647087c34f81515401b19d310a98ccceaad7
sagar <sagar@taglr.com>
2020-03-22 17:16:10 +0530

    react

---
+++ precache-manifest.89eec0c8ab38814e33608b6e301025de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ea8810e1803191f2a3bad7cc5e243c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f97bb66f22343773c35",
+    "url": "/static/css/main.2dd1bc36.chunk.css"
+  },
+  {
+    "revision": "eb1d9a75c88570f4e746",
+    "url": "/static/js/2.feaeb0f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.feaeb0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f97bb66f22343773c35",
+    "url": "/static/js/main.5a49b622.chunk.js"
+  },
+  {
+    "revision": "e09b3a133424f3eaaad2",
+    "url": "/static/js/runtime-main.d7a593f2.js"
+  }
+]);
